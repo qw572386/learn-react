ea22d9d5895a824ca4b05e7f1b622ffaac2caaaa
qw572386 <735520121@qq.com>
2019-05-27 23:24:58 +0800

    使用redux完成antd todolist

--- src/ant_todo/index.js
+++ src/ant_todo/index.js
@@ -6,24 +6,56 @@ class TodoList extends Component {
   constructor(props) {
     super(props);
     this.state = store.getState();
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleStoreChange = this.handleStoreChange.bind(this);
+    this.handleBtnClick = this.handleBtnClick.bind(this);
+    store.subscribe(this.handleStoreChange)
   }
   render() {
     return (
       <div style={{margin: "10px 0 0 10px"}}>
-        <Input placeholder="please input" value={this.state.inputValue} style={{width: "300px", marginRight: "10px"}}/>
-        <Button type="primary">提交</Button>
+        <Input 
+          placeholder="please input" 
+          value={this.state.inputValue} 
+          style={{width: "300px", marginRight: "10px"}}
+          onChange={this.handleInputChange}
+        />
+        <Button type="primary" onClick={this.handleBtnClick}>提交</Button>
         <List
           style={{marginTop: "10px", width: "300px"}}
           bordered
           dataSource={this.state.list}
-          renderItem={item => (
-            <List.Item>
+          renderItem={(item, index) => (
+            <List.Item onClick={this.handleItemDelete.bind(this, index)}>
               {item}
             </List.Item>
           )}
         />
       </div>
     )
+  }
+  handleInputChange(e) {
+    const action = {
+      type: 'change_input_value',
+      value: e.target.value
+    }
+    store.dispatch(action);
+  }
+  handleStoreChange() {
+    this.setState(store.getState())
+  }
+  handleBtnClick() {
+    const action = {
+      type: 'add_todo_item'
+    };
+    store.dispatch(action)
+  }
+  handleItemDelete(index) {
+    const action = {
+      type: 'delete_todo_item',
+      index
+    }
+    store.dispatch(action);
   }
 }
 export default TodoList;
